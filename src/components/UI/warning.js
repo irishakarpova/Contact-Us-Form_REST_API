94c4a24abe53146fe6d03c5a4010bd0bdf7908d3
irina <irisha.karpova@gmail.com>
2020-08-07 12:59:00 -0500

    warning msg

--- src/components/UI/warning.js
+++ src/components/UI/warning.js
@@ -8,7 +8,6 @@ export default (props) => {
       <div className='warning-msg'></div>
       <div className='d-flex flex-column align-items-center justify-content-center alert-warning col-md-10 m-auto p-0'>
         <div className='icon-msg-wng'></div>
-        {props.message}
         <p>Sorry <br/> your specific request <br/> is temporarily inaccessible.</p>
         <button className='button-warning-border'
                 onClick={props.handleClickClose}
