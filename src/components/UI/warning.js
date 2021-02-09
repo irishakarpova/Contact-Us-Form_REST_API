93ca0149ac671495e249044bdf1e8e23afc05162
irina <irisha.karpova@gmail.com>
2021-02-09 14:46:20 -0600

    style update

--- src/components/UI/warning.js
+++ src/components/UI/warning.js
@@ -9,7 +9,7 @@ export default (props) => {
       <div className='d-flex flex-column align-items-center justify-content-center alert-warning col-md-10 m-auto p-0'>
         <div className='icon-msg-wng'></div>
         <p>Sorry <br/> your specific request <br/> is temporarily inaccessible.</p>
-        <button className='button-warning-border'
+        <button className='button-warning'
                 onClick={props.handleClickClose}
         >
           HIDE
